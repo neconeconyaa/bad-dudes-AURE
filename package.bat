a536a261e114bb333c64d2460501ee390f0efb4e
Neco Neco-Nyaa <NecoNecoNyaaDev@gmail.com>
2020-09-18 15:41:09 -0700

    fix release builder

--- package.bat
+++ package.bat
@@ -1,7 +1,7 @@
 mkdir release
-mkdir release\package
-mkdir release\package\bin
-copy bin\* release\package\bin
-copy install.bat release\package
-copy README-RELEASE.md release\package
-tar -cf release\bad-dudes-among-us-radio-environment.tar --directory=release\package *
+mkdir release\bd-acur
+mkdir release\bd-acur\bin
+copy bin\* release\bd-acur\bin
+copy install.bat release\bd-acur
+copy README-RELEASE.md release\bd-acur
+tar -cf release\bad-dudes-among-us-radio-environment.tar --directory=release bd-acur
